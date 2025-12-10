e6ad80bd026f881ea8748e565ec7fc7898cb3b60
Florian Rieder <florian.rieder@unil.ch>
2025-12-10 22:17:28 +0100

    feat: implement Keep-Alive

--- httpd.go
+++ httpd.go
@@ -24,6 +24,8 @@ import (
 
 const DOCUMENT_ROOT = "/Users/frieder/Documents/go/httpd/html"
 const PORT = 8080
+const TIMEOUT = 5
+const MAX_REQUESTS = 100
 
 func main() {
 
@@ -51,103 +53,120 @@ func handleConnection(conn net.Conn) {
 	// Use bufio to read the connection's data line by line without too much effort
 	reader := bufio.NewReader(conn)
 
-	status := 200
+	// Set initial timeout
+	conn.SetReadDeadline(time.Now().Add(TIMEOUT * time.Second))
 
-	// Read the Request-Line (e.g., "GET /hello.txt HTTP/1.1")
-	requestLine, err := reader.ReadString('\n')
-	if err != nil {
-		if err != io.EOF {
-			log.Printf("Error reading from connection: %v", err)
-		} else {
-			log.Printf("Unexpected EOF in request-line")
+	// Loop requests over the connection for Keep-Alive
+	for requestCounter := 0; requestCounter < MAX_REQUESTS; requestCounter++ {
+		status := 200
+
+		// Read the Request-Line (e.g., "GET /hello.txt HTTP/1.1")
+		requestLine, err := reader.ReadString('\n')
+		if err != nil {
+			if err != io.EOF {
+				log.Printf("Error reading from connection: %v", err)
+			} else {
+				log.Printf("Unexpected EOF in request-line")
+			}
 		}
-	}
 
-	// Strip newline
-	requestLine = strings.TrimSpace(requestLine)
-	// Split the string into a slice of strings
-	parts := strings.Split(requestLine, " ")
+		// Strip newline
+		requestLine = strings.TrimSpace(requestLine)
 
-	// Check the length of the resulting slice
-	if len(parts) < 3 {
-		fmt.Println("Error: Invalid request line format.")
-		return
-	}
-
-	method := parts[0] // For now only GET is supported
-	rawEncodedPath := parts[1]
-	httpVersion := parts[2]
-
-	if method != "GET" {
-		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
-		log.Printf("Unsupported method: '%s'", method)
-		return
-	}
-	if httpVersion != "HTTP/1.1" {
-		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
-		log.Printf("Unsupported HTTP Version: '%s'", httpVersion)
-		return
-	}
-
-	headers := readHeaders(reader)
-
-	fmt.Printf("Received Headers (Debug Format):\n%v\n", headers)
-
-	// Decode path
-	decodedPath, err := url.QueryUnescape(rawEncodedPath)
-	if err != nil {
-		conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
-		return
-	}
-
-	if decodedPath == "/" {
-		// default to index.html
-		decodedPath = "/index.html"
-	}
-
-	contentType := "text/plain"
-	cleanPath := filepath.Clean(decodedPath)
-	localFilePath := filepath.Join(DOCUMENT_ROOT, cleanPath)
-
-	fmt.Printf(localFilePath)
-
-	fileContent, err := os.ReadFile(localFilePath)
-	if err != nil {
-		if os.IsNotExist(err) {
-			status = 404
-			fileContent = []byte("<h1>404 Not Found</h1>")
-			contentType = "text/html"
-		} else {
-			// e.g., permission error
-			status = 500
-			fileContent = []byte("<h1>500 Internal Server Error</h1>")
-			contentType = "text/html"
-			log.Printf("File I/O Error: %v", err)
+		if requestLine == "" {
+			break
 		}
-	} else {
-		// Determine MIME type from file extension
-		ext := filepath.Ext(localFilePath)
-		contentType = mime.TypeByExtension(ext)
-		status = 200
+
+		// Split the string into a slice of strings
+		parts := strings.Split(requestLine, " ")
+
+		// Check the length of the resulting slice
+		if len(parts) < 3 {
+			fmt.Println("Error: Invalid request line format.")
+			return
+		}
+
+		method := parts[0] // For now only GET is supported
+		rawEncodedPath := parts[1]
+		httpVersion := parts[2]
+
+		if method != "GET" {
+			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			log.Printf("Unsupported method: '%s'", method)
+			return
+		}
+		if httpVersion != "HTTP/1.1" {
+			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			log.Printf("Unsupported HTTP Version: '%s'", httpVersion)
+			return
+		}
+
+		headers := readHeaders(reader)
+
+		// Decode path
+		decodedPath, err := url.QueryUnescape(rawEncodedPath)
+		if err != nil {
+			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n"))
+			return
+		}
+
+		if decodedPath == "/" {
+			// default to index.html
+			decodedPath = "/index.html"
+		}
+
+		contentType := "text/plain"
+		cleanPath := filepath.Clean(decodedPath)
+		localFilePath := filepath.Join(DOCUMENT_ROOT, cleanPath)
+
+		fileContent, err := os.ReadFile(localFilePath)
+		if err != nil {
+			if os.IsNotExist(err) {
+				status = 404
+				fileContent = []byte("<h1>404 Not Found</h1>")
+				contentType = "text/html"
+			} else {
+				// e.g., permission error
+				status = 500
+				fileContent = []byte("<h1>500 Internal Server Error</h1>")
+				contentType = "text/html"
+				log.Printf("File I/O Error: %v", err)
+			}
+		} else {
+			// Determine MIME type from file extension
+			ext := filepath.Ext(localFilePath)
+			contentType = mime.TypeByExtension(ext)
+			status = 200
+		}
+
+		// Access log
+		fmt.Printf("[%s] %s - %s %d - %s\n", time.Now().Format(time.RFC3339), conn.RemoteAddr(), method, status, decodedPath)
+
+		responseHeaders := http.Header{}
+		responseHeaders.Add("Server", "flo's mini httpd")
+		responseHeaders.Add("Date", time.Now().UTC().Format(time.RFC1123))
+		responseHeaders.Add("Content-Type", contentType)
+
+		// Set keep-alive header
+		if headers.Get("Connection") == "keep-alive" {
+			responseHeaders.Add("Connection", "keep-alive")
+			responseHeaders.Add("Keep-Alive", fmt.Sprintf("timeout=%d, max=%d", TIMEOUT, MAX_REQUESTS))
+		} else {
+			responseHeaders.Add("Connection", "close")
+		}
+
+		response := buildResponse(status, fileContent, responseHeaders)
+
+		conn.Write([]byte(response))
+
+		// Close the connection if the Connection header is set to "close" or missing
+		if headers.Get("Connection") == "close" || headers.Get("Connection") == "" {
+			break
+		}
+
+		// Set additional timeout to wait for the next request
+		conn.SetReadDeadline(time.Now().Add(TIMEOUT * time.Second))
 	}
-
-	// Access log
-	fmt.Printf("[%s] %s - %s %d - %s\n", time.Now().Format(time.RFC3339), conn.RemoteAddr(), method, status, decodedPath)
-
-	response := fmt.Sprintf("HTTP/1.1 %d %s\r\n"+
-		"Server: flo's mini httpd\r\n"+
-		"Content-Type: %s\r\n"+
-		"Date: %s\r\n"+
-		"Content-Length: %d\r\n"+
-		"\r\n%s",
-		status,
-		http.StatusText(status),
-		contentType,
-		time.Now().UTC().Format(time.RFC1123),
-		len(fileContent),
-		string(fileContent))
-
-	conn.Write([]byte(response))
 }
 
 func readHeaders(reader *bufio.Reader) http.Header {
@@ -192,4 +211,23 @@ func readHeaders(reader *bufio.Reader) http.Header {
 	}
 
 	return headers
+}
+
+func buildResponse(status int, body []byte, responseHeaders http.Header) []byte {
+	header := fmt.Sprintf("HTTP/1.1 %d %s\r\n"+
+		"Content-Length: %d\r\n",
+		status,
+		http.StatusText(status),
+		len(body),
+	)
+
+	if responseHeaders != nil {
+		for key, value := range responseHeaders {
+			header += fmt.Sprintf("%s: %s\r\n", key, value[0])
+		}
+	}
+
+	header += "\r\n"
+
+	return append([]byte(header), body...)
 }
