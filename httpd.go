e4a1cab1199b9b564f3bf496fb4b1d7d3fc56186
Florian Rieder <florian.rieder@unil.ch>
2025-12-14 01:01:29 +0100

    refactor: extract response generation to separate function

--- httpd.go
+++ httpd.go
@@ -12,16 +12,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"strings"
 	"time"
 )
-
-const DOCUMENT_ROOT = "/Users/frieder/Documents/go/httpd/html"
-const PORT = 8080
-const TIMEOUT = 10
-const MAX_REQUESTS = 100
 
 func main() {
 
@@ -58,7 +51,6 @@ func handleConnection(conn net.Conn, config Config) {
 
 	// Loop requests over the connection for Keep-Alive
 	for requestCounter := 0; requestCounter < config.max_requests_per_connection; requestCounter++ {
-
 		// 1) Parse the incoming request
 		request, err := parseRequest(reader)
 		if err != nil {
@@ -67,100 +59,28 @@ func handleConnection(conn net.Conn, config Config) {
 				return
 			}
 			log.Printf("Error parsing request: %v", err)
-			conn.Write([]byte("HTTP/1.1 400 Bad Request\r\n\r\n<h1>400 Bad Request</h1>\r\n\r\n"))
+			serveErrorDocument(conn, 400) // Bad Request
 			return
 		}
 
 		// 2) Get information about the targeted resource
 		resourceInfo, err := getResourceInfo(request.Path, config)
 		if os.IsNotExist(err) {
-			log.Printf("404 found by getResourceInfo: %v", err)
-			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n<h1>404 Not Found</h1>\r\n\r\n"))
+			serveErrorDocument(conn, 404) // Not Found
 			return
 		}
-		// if os.IsPermission(err) {
-		// 	conn.Write([]byte("HTTP/1.1 403 Forbidden\r\n\r\n<h1>403 Forbidden</h1>\r\n\r\n"))
-		// 	return
-		// }
+		if os.IsPermission(err) {
+		 	serveErrorDocument(conn, 404) // Forbidden, but maybe don't tell them ?
+			return
+		}
 		if err != nil {
 			log.Printf("Error getting resource info: %v", err)
-			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n<h1>500 Internal Server Error</h1>\r\n\r\n"))
+			serveErrorDocument(conn, 500) // Internal Server Error
 			return
 		}
-
-		fmt.Printf("Resource Info: %+v\n", resourceInfo)
 
 		// 3) Build the response
-		encoding := "identity"
-		status := 200
-
-		fileContent, err := os.ReadFile(resourceInfo.LocalFilePath)
-		if err != nil {
-			conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n<h1>500 Internal Server Error</h1>\r\n\r\n"))
-			return
-		}
-		// The client sends the header "If-None-Match" with the last Etag they have in memory for this URL
-		// The server compares the client's ETag (sent with If-None-Match) with the ETag for its
-		// current version of the resource, and if both values match (that is, the resource has
-		// not changed), the server sends back a 304 Not Modified status, without a body, which
-		// tells the client that the cached version of the response is still good to use (fresh).
-		// see https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/ETag
-		clientEtag := request.Headers.Get("If-None-Match")
-		if clientEtag != "" {
-			if clientEtag == resourceInfo.ETag {
-				// Yup, we got the same file, just use that !
-				status = 304
-				fileContent = []byte("") // Ensure no body is sent
-			}
-		}
-
-		// Check if the client accepts gzip compression
-		acceptedEncodings := strings.Split(request.Headers.Get("Accept-Encoding"), ",")
-		for _, acceptedEncoding := range acceptedEncodings {
-			acceptedEncoding = strings.TrimSpace(acceptedEncoding)
-			if acceptedEncoding == "gzip" {
-				compressedContent, err := gzipBytes(fileContent)
-				if err != nil {
-					log.Printf("Error compressing response: %v", err)
-				}
-				fileContent = compressedContent
-				encoding = "gzip"
-				break
-			}
-		}
-
-		// Access log
-		fmt.Printf("[%s] %d %s %s - %s (%s)\n", time.Now().Format(time.RFC3339), status, request.Method, request.Path, conn.RemoteAddr(), request.Headers.Get("User-Agent"))
-
-		now := time.Now().UTC().Format(time.RFC1123)
-		responseHeaders := http.Header{}
-		responseHeaders.Add("Server", "flo's mini httpd")
-		responseHeaders.Add("Date", now)
-		responseHeaders.Add("Expires", now)
-		responseHeaders.Add("Content-Type", resourceInfo.ContentType)
-
-		if resourceInfo.ETag != "" {
-			responseHeaders.Add("ETag", resourceInfo.ETag)
-		}
-
-		// If the file was compressed, add the appropriate headers to tell the client
-		// how to decompress it.
-		if encoding == "gzip" {
-			responseHeaders.Add("Content-Encoding", "gzip")
-			responseHeaders.Add("Vary", "Accept-Encoding")
-		}
-
-		// Set keep-alive header to notify the client that the connection is still open and can be reused
-		if request.Headers.Get("Connection") == "keep-alive" {
-			responseHeaders.Add("Connection", "keep-alive")
-			responseHeaders.Add("Keep-Alive", fmt.Sprintf("timeout=%d, max=%d", config.timeout, config.max_requests_per_connection))
-		} else {
-			responseHeaders.Add("Connection", "close")
-		}
-
-		response := buildResponse(status, fileContent, responseHeaders)
-
-		conn.Write([]byte(response))
+		serveBufferedResponse(conn, config, request, resourceInfo)
 
 		// Close the connection if the Connection header is set to "close" or missing
 		if request.Headers.Get("Connection") == "close" || request.Headers.Get("Connection") == "" {
--- response.go
+++ response.go
@@ -2,8 +2,86 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
+
+func serveBufferedResponse(conn net.Conn, config Config, request Request, resourceInfo ResourceInfo) {
+	encoding := "identity"
+	status := 200
+
+	fileContent, err := os.ReadFile(resourceInfo.LocalFilePath)
+	if err != nil {
+		conn.Write([]byte("HTTP/1.1 500 Internal Server Error\r\n\r\n<h1>500 Internal Server Error</h1>\r\n\r\n"))
+		return
+	}
+	// The client sends the header "If-None-Match" with the last Etag they have in memory for this URL
+	// The server compares the client's ETag (sent with If-None-Match) with the ETag for its
+	// current version of the resource, and if both values match (that is, the resource has
+	// not changed), the server sends back a 304 Not Modified status, without a body, which
+	// tells the client that the cached version of the response is still good to use (fresh).
+	// see https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/ETag
+	clientEtag := request.Headers.Get("If-None-Match")
+	if clientEtag != "" {
+		if clientEtag == resourceInfo.ETag {
+			// Yup, we got the same file, just use that !
+			status = 304
+			fileContent = []byte("") // Ensure no body is sent
+		}
+	}
+
+	// Check if the client accepts gzip compression
+	acceptedEncodings := strings.Split(request.Headers.Get("Accept-Encoding"), ",")
+	for _, acceptedEncoding := range acceptedEncodings {
+		acceptedEncoding = strings.TrimSpace(acceptedEncoding)
+		if acceptedEncoding == "gzip" {
+			compressedContent, err := gzipBytes(fileContent)
+			if err != nil {
+				log.Printf("Error compressing response: %v", err)
+			}
+			fileContent = compressedContent
+			encoding = "gzip"
+			break
+		}
+	}
+
+	// Access log
+	fmt.Printf("[%s] %d %s %s - %s (%s)\n", time.Now().Format(time.RFC3339), status, request.Method, request.Path, conn.RemoteAddr(), request.Headers.Get("User-Agent"))
+
+	now := time.Now().UTC().Format(time.RFC1123)
+	responseHeaders := http.Header{}
+	responseHeaders.Add("Server", "flo's mini httpd")
+	responseHeaders.Add("Date", now)
+	responseHeaders.Add("Expires", now)
+	responseHeaders.Add("Content-Type", resourceInfo.ContentType)
+
+	if resourceInfo.ETag != "" {
+		responseHeaders.Add("ETag", resourceInfo.ETag)
+	}
+
+	// If the file was compressed, add the appropriate headers to tell the client
+	// how to decompress it.
+	if encoding == "gzip" {
+		responseHeaders.Add("Content-Encoding", "gzip")
+		responseHeaders.Add("Vary", "Accept-Encoding")
+	}
+
+	// Set keep-alive header to notify the client that the connection is still open and can be reused
+	if request.Headers.Get("Connection") == "keep-alive" {
+		responseHeaders.Add("Connection", "keep-alive")
+		responseHeaders.Add("Keep-Alive", fmt.Sprintf("timeout=%d, max=%d", config.timeout, config.max_requests_per_connection))
+	} else {
+		responseHeaders.Add("Connection", "close")
+	}
+
+	response := buildResponse(status, fileContent, responseHeaders)
+
+	conn.Write([]byte(response))
+}
 
 func buildResponse(status int, body []byte, responseHeaders http.Header) []byte {
 	header := fmt.Sprintf("HTTP/1.1 %d %s\r\n"+
@@ -23,4 +101,18 @@ func buildResponse(status int, body []byte, responseHeaders http.Header) []byte 
 	// Combine header and body
 	response := append([]byte(header), body...)
 	return response
+}
+
+func serveErrorDocument(conn net.Conn, status int) {
+	statusText := http.StatusText(status)
+	body := fmt.Sprintf("<h1>%d %s</h1>\r\n", status, statusText)
+
+	responseHeaders := http.Header{}
+	responseHeaders.Add("Server", "flo's mini httpd")
+	responseHeaders.Add("Date", time.Now().UTC().Format(time.RFC1123))
+	responseHeaders.Add("Content-Type", "text/html")
+	responseHeaders.Add("Connection", "close")
+
+	response := buildResponse(status, []byte(body), responseHeaders)
+	conn.Write([]byte(response))
 }
